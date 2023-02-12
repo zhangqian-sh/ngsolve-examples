474396ca52bdf3f9c9efc134c614b8329a4d37a2
Qian Zhang <zhangqian_98sh@outlook.com>
2023-02-12 18:21:51 -0500

    Add 1d mesh of interval [0,1] with Dirichlet and periodic BC

---
+++ src/utils/mesh_1d.py
@@ -0,0 +1,52 @@
+# Get 1D mesh
+from netgen.meshing import *
+import ngsolve
+
+
+def generate_1d_mesh_dirichlet(N_element: int):
+    # generate a 1D mesh
+    m = Mesh(dim=1)
+
+    # add points
+    pnums = []
+    for i in range(0, N_element + 1):
+        pnums.append(m.Add(MeshPoint(Pnt(i / N_element, 0, 0))))
+
+    # add element
+    idx = m.AddRegion("medium", dim=1)
+    for i in range(0, N_element):
+        m.Add(Element1D([pnums[i], pnums[i + 1]], index=idx))
+
+    # add boundary condition
+    idx_left = m.AddRegion("left", dim=0)
+    idx_right = m.AddRegion("right", dim=0)
+
+    m.Add(Element0D(pnums[0], index=idx_left))
+    m.Add(Element0D(pnums[N_element], index=idx_right))
+
+    # create mesh
+    mesh = ngsolve.Mesh(m)
+    return mesh
+
+
+def generate_1d_mesh_periodic(N_element: int):
+    # generate a 1D mesh
+    m = Mesh(dim=1)
+
+    # add points
+    pnums = []
+    for i in range(0, N_element + 1):
+        pnums.append(m.Add(MeshPoint(Pnt(i / N_element, 0, 0))))
+
+    # add element
+    idx = m.AddRegion("medium", dim=1)
+    for i in range(0, N_element):
+        m.Add(Element1D([pnums[i], pnums[i + 1]], index=idx))
+
+    m.Add(Element0D(pnums[0], index=1))
+    m.Add(Element0D(pnums[N_element], index=2))
+    m.AddPointIdentification(pnums[0], pnums[N_element], 1, 2)
+
+    # create mesh
+    mesh = ngsolve.Mesh(m)
+    return mesh
